ac874ca22e94ae999d8a9c0643514bf24e07caa7
Arahk Yambupah <arahk@outlook.com>
2024-06-08 09:41:02 +0700

    Improve Reminder List UI

--- Arahk.Reminder.Mobile/Services/ReminderService.cs
+++ Arahk.Reminder.Mobile/Services/ReminderService.cs
@@ -7,14 +7,40 @@ public class ReminderService
 {    
     public async Task<IEnumerable<ReminderListItemModel>> ListReminderAsync(CancellationToken cancellationToken)
     {
-        var httpClient = new HttpClient();
-        var httpRequest = new HttpRequestMessage(HttpMethod.Get, new Uri("http://localhost:5182/Reminder"));
-        var httpResponse = await httpClient.SendAsync(httpRequest, cancellationToken);
+        try
+        {
+            var httpClient = new HttpClient();
+            var httpRequest = new HttpRequestMessage(HttpMethod.Get, new Uri("http://localhost:5182/Reminder"));
+            // var httpRequest = new HttpRequestMessage(HttpMethod.Get, new Uri("https://arahk-reminder.azurewebsites.net/Reminder"));
+            var httpResponse = await httpClient.SendAsync(httpRequest, cancellationToken);
 
-        httpResponse.EnsureSuccessStatusCode();
+            httpResponse.EnsureSuccessStatusCode();
 
-        var reminderItems = await httpResponse.Content.ReadFromJsonAsync<IEnumerable<ReminderListItemModel>>(cancellationToken) ?? [];
+            var reminderItems = await httpResponse.Content.ReadFromJsonAsync<IEnumerable<ReminderListItemModel>>(cancellationToken) ?? [];
 
-        return reminderItems;
+            return reminderItems;
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+    }
+
+    public async Task<IEnumerable<ReminderListItemModel>> ListReminderAsyncMock(CancellationToken cancellationToken)
+    {
+        List<ReminderListItemModel> _models = [];
+        
+        for (var i = 1; i <= 20; i++)
+        {
+            await Task.Delay(10, cancellationToken);
+            
+            _models.Add(new ReminderListItemModel()
+            {
+                Title = $"Reminder Item {i}",
+                Subtitle = "Created by Someone"
+            });
+        }
+
+        return _models;
     }
 }
